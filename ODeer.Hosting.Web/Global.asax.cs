dcf9b24e0c540e73312b013d06d3e99a83f1749b
daywrite <daywrite@yeah.net>
2014-08-13 13:39:13 +0800

    增加Plugin插件，并添加一个Admin

--- ODeer.Hosting.Web/Global.asax.cs
+++ ODeer.Hosting.Web/Global.asax.cs
@@ -1,21 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+using ODeer.Plugin.Mvc;
+
+using UIShell.NavigationService;
+
+
 namespace ODeer.Hosting.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : BundleRuntimeMvcApplication
     {
-        protected void Application_Start()
+        public static IEnumerable<NavigationNode> NavigationNodes { get; set; }
+
+        protected override void Application_Start(object sender, EventArgs e)
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Assembly hostAssembly = Assembly.GetExecutingAssembly();
+            Assembly[] hostDependencyAssemblies = hostAssembly.GetReferencedAssemblies()
+                .Where(m => m.Name.Contains("ODeer")).Select(Assembly.Load).ToArray();
+            IoCBundleActivator.HostingAssemblyFunc = () => new[] { hostAssembly }.Union(hostDependencyAssemblies).ToArray();
+            DefaultConfig.RegisterHostingNamespaces(hostAssembly);
+
+            base.Application_Start(sender, e);
+
+            //DatabaseInitializer.Initialize();
+
+            NavigationNodes = NavigationInitialize();
+        }
+
+        public override void RegisterResourceBundles()
+        {
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public override void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            //filters.Add(new HandleAjaxErrorAttribute());
+            base.RegisterGlobalFilters(filters);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = HttpContext.Current.Server.GetLastError();
+        }
+
+        private IEnumerable<NavigationNode> NavigationInitialize()
+        {
+            INavigationService service = BundleRuntime.GetFirstOrDefaultService<INavigationService>();
+            if (service == null)
+            {
+                return new List<NavigationNode>();
+            }
+            return service.NavgationNodes;
         }
     }
 }
---
+++ ODeer.Hosting.Web/Plugins/ODeer.Plugin.Admin/BundleActivator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ODeer.Plugin.Mvc;
+
+using UIShell.OSGi;
+using UIShell.PageFlowService;
+
+namespace ODeer.Plugin.Admin
+{
+    public class BundleActivator : IBundleActivator
+    {
+        public static IBundle Bundle { get; private set; }
+
+        public static ServiceTracker<IPageFlowService> PageFlowServiceTracker { get; set; }
+
+        public static PageNode LayoutPageNode
+        {
+            get { return PageFlowServiceTracker.DefaultOrFirstService.GetPageNode("LayoutPage"); }
+        }
+
+        public static PageNode GridLayoutPageNode
+        {
+            get { return PageFlowServiceTracker.DefaultOrFirstService.GetPageNode("GridLayoutPage"); }
+        }
+
+        public void Start(IBundleContext context)
+        {
+            Bundle = context.Bundle;
+            PageFlowServiceTracker = new ServiceTracker<IPageFlowService>(context);
+            DefaultConfig.RegisterBundleNamespaces(context.Bundle.SymbolicName, GetType().Assembly);
+        }
+
+        public void Stop(IBundleContext context)
+        { }
+    }
+}
